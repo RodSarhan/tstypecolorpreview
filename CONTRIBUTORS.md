9ab84f9ccd793605dc39643bb8025424682dde54
Roudain Sarhan <46911303+RodSarhan@users.noreply.github.com>
2024-06-24 23:32:06 +0800

    use ts server instead of ts-morph (#1)
    * use ts server instead of ts-morph

---
+++ CONTRIBUTORS.md
@@ -0,0 +1,5 @@
+# Contributors
+
+This file lists all the people who have contributed to this project. Thanks!
+
+- [Myles Murphy](https://github.com/mylesmmurphy)
---
+++ packages/vscode-extension/src/get-colors-from-type.ts
@@ -0,0 +1,61 @@
+import type { TypeTree } from './types'
+import { converter, formatHex8 } from 'culori'
+const convertToRGB = converter('rgb')
+
+type ColorObject = {
+  name: string
+  hexValue: string
+  transparency: number | undefined
+}
+
+// This is working for all react native supported color formats except
+// const didntWork: {
+//     number1: 'hwb(0, 0%, 22%)'; //Other HWB formats work
+//     number2: 'hwb(360, 33%, 22%)'; //Other HWB formats work
+//     number3: '0xff00ff00'; //int not working
+// };
+
+/**
+ * Uses type info to return a string representation of the type
+ *
+ * Example:
+ * { kind: 'union', types: [{ kind: 'string-literal', type: '"#000000"' }, { kind: 'string-literal', type: '"#FFFFFF"' }] }
+ * Yields:
+ * ColorObject
+ */
+export function getColorsFromType (typeTree: TypeTree): ColorObject[] | undefined {
+  const colorsList: ColorObject[] = []
+
+  if (typeTree.kind === 'union') {
+    typeTree.types.forEach((type) => {
+      if (type.kind === 'string-literal') {
+        const cleanString = type.typeName.replace(/"/g, '')
+        const colorObject = getColorObjectFromType(cleanString)
+        if (colorObject) colorsList.push(colorObject)
+      }
+    })
+  }
+
+  if (typeTree.kind === 'string-literal') {
+    const cleanString = typeTree.typeName.replace(/"/g, '')
+    const colorObject = getColorObjectFromType(cleanString)
+    if (colorObject) colorsList.push(colorObject)
+  }
+
+  return colorsList.length > 0 ? colorsList : undefined
+}
+
+const getColorObjectFromType = (type: string): {
+  name: string
+  hexValue: string
+  transparency: number | undefined
+} | undefined => {
+  const rgbFromString = convertToRGB(type)
+  if (rgbFromString) {
+    return {
+      name: type,
+      hexValue: formatHex8(rgbFromString),
+      transparency: rgbFromString.alpha
+    }
+  }
+}
